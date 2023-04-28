ed0d553314fd470aac93d5f04914f481689feca0
Alexey Ushakov <kafka.pochta@gmail.com>
2023-04-28 20:30:00 +0200

    update

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# woot-webhook
+# woothook - Chatwoot webhook server using woot package
 
-Example project can be used as a template for chatwoot webhook server that performs via Chatwoot API (woot package).
+Example project can be used as a template for Chatwoot webhook server that performs via Chatwoot API ([woot](https://github.com/dearkafka/woot) package).
 
 It listens for any telegram messages in Chatwoot and adds Telegram user name (e.g. @durov) to Contatct Attributes field "telegram".
 
 However, you are free to use it as an example and template for adding any Chatwoot API related  functionality, like connecting Google Spreadsheets, Airtable, etc.
+
+Also it has docker support so you can build it and include into yours docker-compose file.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+def long_description():
+    with open("README.md") as readme:
+        return readme.read()
+
+
+setup(
+    name="woothook",
+    version="0.0.1",
+    author="Aleksei Ushakov",
+    author_email="isfluid@proton.me",
+    description="Chatwoot webhook server template using woot",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    packages=["woothook"],
+    license="Cooperative Non-Violent Public License v7 or later (CNPLv7+)",
+    project_urls={
+        "Bug Tracker": "https://github.com/dearkafka/woot-service/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=["requests>=2.27.1", "woot>=0.0.1", "fastapi", "uvicorn"],
+)
